78c05b6ee35adb1ffb3fa6e720dd55c685a45bcb
Deepak Choudhary <gljat999@gmail.com>
2019-10-25 03:10:58 +0530

    added

---
+++ Exp1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{   
+    int T;
+    char s[10000];
+    scanf("%d", &n);
+
+    for (int i = 1; i <= T; i++)
+    {
+        scanf("%s", &s);
+        if (/* condition */)
+        {
+            /* code */
+        }
+        
+        printf("%s", )
+    }
+    
+    
+    scanf("%s", &n);
+    
+    return 0;  
+}
