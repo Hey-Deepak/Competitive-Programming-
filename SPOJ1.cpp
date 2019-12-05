aca1c266eb0a301d120ec102a52ac305f9bf79c6
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-05 23:19:22 +0530

    Create SPOJ1.cpp

---
+++ SPOJ1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    unsigned short num1, num2;
+    unsigned short sum;
+
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << sum;
+ 
+    return 0;
+}
