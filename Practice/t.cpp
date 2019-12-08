743e88f34c18e890ab2d20f88b5b020729cac48e
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-09 02:32:24 +0530

    while without condition
    give an error

--- Practice/t.cpp
+++ Practice/t.cpp
@@ -10,7 +10,7 @@ int main(){
     {
         if (!((line[i] >= 'a' && line[i]<='z') || (line[i] >= 'A' && line[i]<='Z')))
         {
-            line[i] = '\0';
+            line[i] = ' ';
         }
     }
     cout << "Output String: " << line;    
