cf9cc6f8171d9d5cef70c8c77d6e63c9aaad9c54
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-09 04:00:14 +0530

    code reduced

--- SimpleArraySum.cpp
+++ SimpleArraySum.cpp
@@ -3,18 +3,14 @@ using namespace std;
 
 int main(){
     int array[1000], input, sum = 0;
-
     cin >> input ;
+   
     for ( int i = 0; i < input; i++ ) 
     {
         cin >> array[i];
-    }
-
-    for ( int i = 0; i < input; i++ )
-    {
         sum += array[i];
     }
-    
+   
     cout << sum << endl;
     return 0;
 }
